61a0a9e7b68926fff6b9ec89125d93b9b42b6e36
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2021-02-09 18:42:47 +0200

    Update

--- src/history.java
+++ src/history.java
@@ -11,8 +11,8 @@ public class history extends JFrame implements MouseListener{
     JLabel[] label;
     JTextField textfield;
     JTable table;
-    Color c,c1;
-    Font f,f1,f2;
+    Color color1,color2;
+    Font font1,f1,f2;
     Connection conn;
     PreparedStatement ps = null;
     ResultSet rs = null;
@@ -30,19 +30,19 @@ public class history extends JFrame implements MouseListener{
         table = new JTable();
         label = new JLabel[20];
         textfield = new JTextField();
-        c = new Color(0, 102, 102);
-        c1 = new Color(211, 84, 0);
-        f = new Font("seirf", Font.BOLD, 22);
+        color1 = new Color(0, 102, 102);
+        color2 = new Color(211, 84, 0);
+        font1 = new Font("seirf", Font.BOLD, 22);
         f1 = new Font("seirf", Font.BOLD, 25);
         f2 = new Font("seirf", Font.BOLD, 18);
         
-        panel.setBackground(c);
+        panel.setBackground(color1);
         panel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         panel.setLayout(null);
         this.add(panel);
         
         textfield.setBounds(440, 145, 680, 35);
-        textfield.setFont(f);
+        textfield.setFont(font1);
         panel.add(textfield);
         
         table.setBounds(30, 300, 1304, 410);
@@ -55,59 +55,59 @@ public class history extends JFrame implements MouseListener{
         panel.add(label[0]);
         
         label[1] = new JLabel("   X");
-        label[1].setBackground(c);
+        label[1].setBackground(color1);
         label[1].setOpaque(true);
         label[1].setForeground(Color.WHITE);
         label[1].setBounds(1315, 1, 50, 30);
-        label[1].setFont(f);
+        label[1].setFont(font1);
         panel.add(label[1]);
         label[1].addMouseListener(this);
         
         label[2] = new JLabel("  ---");
-        label[2].setBackground(c);
+        label[2].setBackground(color1);
         label[2].setOpaque(true);
         label[2].setForeground(Color.WHITE);
         label[2].setBounds(1265, 1, 50, 30);
-        label[2].setFont(f);
+        label[2].setFont(font1);
         panel.add(label[2]);
         label[2].addMouseListener(this);
         
         label[3] = new JLabel("           Search");
-        label[3].setBackground(c1);
+        label[3].setBackground(color2);
         label[3].setOpaque(true);
         label[3].setForeground(Color.WHITE);
         label[3].setBounds(200, 148, 200, 30);
-        label[3].setFont(f);
+        label[3].setFont(font1);
         panel.add(label[3]);
         label[3].addMouseListener(this);
         
         label[4] = new JLabel("       New Search");
-        label[4].setBackground(c1);
+        label[4].setBackground(color2);
         label[4].setOpaque(true);
         label[4].setForeground(Color.WHITE);
         label[4].setBounds(200, 85, 200, 30);
-        label[4].setFont(f);
+        label[4].setFont(font1);
         panel.add(label[4]);
         label[4].addMouseListener(this);
         
         label[5] = new JLabel("Customer Name:");
-        label[5].setBackground(c);
+        label[5].setBackground(color1);
         label[5].setOpaque(true);
         label[5].setForeground(Color.BLACK);
         label[5].setBounds(440, 215, 175, 30);
-        label[5].setFont(f);
+        label[5].setFont(font1);
         panel.add(label[5]);
         
         label[6] = new JLabel("Phone Number:");
-        label[6].setBackground(c);
+        label[6].setBackground(color1);
         label[6].setOpaque(true);
         label[6].setForeground(Color.BLACK);
         label[6].setBounds(860, 215, 164, 30);
-        label[6].setFont(f);
+        label[6].setFont(font1);
         panel.add(label[6]);
         
         label[7] = new JLabel("");
-        label[7].setBackground(c);
+        label[7].setBackground(color1);
         label[7].setOpaque(true);
         label[7].setForeground(new Color(245, 229, 27));
         label[7].setBounds(620, 217, 220, 30);
@@ -115,7 +115,7 @@ public class history extends JFrame implements MouseListener{
         panel.add(label[7]);
         
         label[8] = new JLabel("");
-        label[8].setBackground(c);
+        label[8].setBackground(color1);
         label[8].setOpaque(true);
         label[8].setForeground(new Color(245, 229, 27));
         label[8].setBounds(1029, 217, 220, 30);
@@ -123,29 +123,29 @@ public class history extends JFrame implements MouseListener{
         panel.add(label[8]);
         
         label[9] = new JLabel("      View History");
-        label[9].setBackground(c1);
+        label[9].setBackground(color2);
         label[9].setOpaque(true);
         label[9].setForeground(Color.WHITE);
         label[9].setBounds(440, 85, 200, 30);
-        label[9].setFont(f);
+        label[9].setFont(font1);
         panel.add(label[9]);
         label[9].addMouseListener(this);
         
         label[10] = new JLabel("         All History");
-        label[10].setBackground(c1);
+        label[10].setBackground(color2);
         label[10].setOpaque(true);
         label[10].setForeground(Color.WHITE);
         label[10].setBounds(680, 85, 200, 30);
-        label[10].setFont(f);
+        label[10].setFont(font1);
         panel.add(label[10]);
         label[10].addMouseListener(this);
         
         label[11] = new JLabel("            Back");
-        label[11].setBackground(c1);
+        label[11].setBackground(color2);
         label[11].setOpaque(true);
         label[11].setForeground(Color.WHITE);
         label[11].setBounds(920, 85, 200, 30);
-        label[11].setFont(f);
+        label[11].setFont(font1);
         panel.add(label[11]);
         label[11].addMouseListener(this);
         
@@ -264,10 +264,10 @@ public class history extends JFrame implements MouseListener{
     @Override
     public void mouseExited(MouseEvent me) {
         if (me.getSource() == label[1]) {
-            label[1].setBackground(c);
+            label[1].setBackground(color1);
         }
         if (me.getSource() == label[2]) {
-            label[2].setBackground(c);
+            label[2].setBackground(color1);
         }
     }
 }
