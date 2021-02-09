b477cf02d1214bb27946c56e88f2d3004f503228
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2021-02-10 00:36:04 +0200

    Update

--- src/historyadmin.java
+++ src/historyadmin.java
@@ -8,10 +8,10 @@ public class historyadmin extends JFrame implements MouseListener{
 
     JPanel panel;
     JLabel[] label;
-    JTextField t;
+    JTextField textfield;
     JTable table;
-    Color c,c1;
-    Font f,f1,f2;
+    Color color1,color2;
+    Font font1,f1,f2;
     Connection conn;
     PreparedStatement ps = null;
     ResultSet rs = null;
@@ -28,21 +28,21 @@ public class historyadmin extends JFrame implements MouseListener{
         panel = new JPanel();
         table = new JTable();
         label = new JLabel[20];
-        t = new JTextField();
-        c = new Color(0, 102, 102);
-        c1 = new Color(211, 84, 0);
-        f = new Font("seirf", Font.BOLD, 22);
+        textfield = new JTextField();
+        color1 = new Color(0, 102, 102);
+        color2 = new Color(211, 84, 0);
+        font1 = new Font("seirf", Font.BOLD, 22);
         f1 = new Font("seirf", Font.BOLD, 25);
         f2 = new Font("seirf", Font.BOLD, 18);
         
-        panel.setBackground(c);
+        panel.setBackground(color1);
         panel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         panel.setLayout(null);
         this.add(panel);
         
-        t.setBounds(360, 145, 680, 35);
-        t.setFont(f);
-        panel.add(t);
+        textfield.setBounds(360, 145, 680, 35);
+        textfield.setFont(font1);
+        panel.add(textfield);
         
         table.setBounds(30, 300, 1304, 410);
         panel.add(table);
@@ -54,59 +54,59 @@ public class historyadmin extends JFrame implements MouseListener{
         panel.add(label[0]);
         
         label[1] = new JLabel("   X");
-        label[1].setBackground(c);
+        label[1].setBackground(color1);
         label[1].setOpaque(true);
         label[1].setForeground(Color.WHITE);
         label[1].setBounds(1315, 1, 50, 30);
-        label[1].setFont(f);
+        label[1].setFont(font1);
         panel.add(label[1]);
         label[1].addMouseListener(this);
         
         label[2] = new JLabel("  ---");
-        label[2].setBackground(c);
+        label[2].setBackground(color1);
         label[2].setOpaque(true);
         label[2].setForeground(Color.WHITE);
         label[2].setBounds(1265, 1, 50, 30);
-        label[2].setFont(f);
+        label[2].setFont(font1);
         panel.add(label[2]);
         label[2].addMouseListener(this);
         
         label[3] = new JLabel("           Search");
-        label[3].setBackground(c1);
+        label[3].setBackground(color2);
         label[3].setOpaque(true);
         label[3].setForeground(Color.WHITE);
         label[3].setBounds(120, 148, 200, 30);
-        label[3].setFont(f);
+        label[3].setFont(font1);
         panel.add(label[3]);
         label[3].addMouseListener(this);
         
         label[4] = new JLabel("       New Search");
-        label[4].setBackground(c1);
+        label[4].setBackground(color2);
         label[4].setOpaque(true);
         label[4].setForeground(Color.WHITE);
         label[4].setBounds(120, 85, 200, 30);
-        label[4].setFont(f);
+        label[4].setFont(font1);
         panel.add(label[4]);
         label[4].addMouseListener(this);
         
         label[5] = new JLabel("Customer Name:");
-        label[5].setBackground(c);
+        label[5].setBackground(color1);
         label[5].setOpaque(true);
         label[5].setForeground(Color.BLACK);
         label[5].setBounds(360, 215, 175, 30);
-        label[5].setFont(f);
+        label[5].setFont(font1);
         panel.add(label[5]);
         
         label[6] = new JLabel("Phone Number:");
-        label[6].setBackground(c);
+        label[6].setBackground(color1);
         label[6].setOpaque(true);
         label[6].setForeground(Color.BLACK);
         label[6].setBounds(780, 215, 164, 30);
-        label[6].setFont(f);
+        label[6].setFont(font1);
         panel.add(label[6]);
         
         label[7] = new JLabel("");
-        label[7].setBackground(c);
+        label[7].setBackground(color1);
         label[7].setOpaque(true);
         label[7].setForeground(new Color(245, 229, 27));
         label[7].setBounds(540, 217, 220, 30);
@@ -114,7 +114,7 @@ public class historyadmin extends JFrame implements MouseListener{
         panel.add(label[7]);
         
         label[8] = new JLabel("");
-        label[8].setBackground(c);
+        label[8].setBackground(color1);
         label[8].setOpaque(true);
         label[8].setForeground(new Color(245, 229, 27));
         label[8].setBounds(949, 217, 220, 30);
@@ -122,29 +122,29 @@ public class historyadmin extends JFrame implements MouseListener{
         panel.add(label[8]);
         
         label[9] = new JLabel("      View History");
-        label[9].setBackground(c1);
+        label[9].setBackground(color2);
         label[9].setOpaque(true);
         label[9].setForeground(Color.WHITE);
         label[9].setBounds(360, 85, 200, 30);
-        label[9].setFont(f);
+        label[9].setFont(font1);
         panel.add(label[9]);
         label[9].addMouseListener(this);
         
         label[10] = new JLabel("         All History");
-        label[10].setBackground(c1);
+        label[10].setBackground(color2);
         label[10].setOpaque(true);
         label[10].setForeground(Color.WHITE);
         label[10].setBounds(600, 85, 200, 30);
-        label[10].setFont(f);
+        label[10].setFont(font1);
         panel.add(label[10]);
         label[10].addMouseListener(this);
         
         label[11] = new JLabel("            Back");
-        label[11].setBackground(c1);
+        label[11].setBackground(color2);
         label[11].setOpaque(true);
         label[11].setForeground(Color.WHITE);
         label[11].setBounds(840, 85, 200, 30);
-        label[11].setFont(f);
+        label[11].setFont(font1);
         panel.add(label[11]);
         label[11].addMouseListener(this);
         
@@ -153,7 +153,7 @@ public class historyadmin extends JFrame implements MouseListener{
         label[12].setOpaque(true);
         label[12].setForeground(Color.WHITE);
         label[12].setBounds(1080, 85, 200, 30);
-        label[12].setFont(f);
+        label[12].setFont(font1);
         panel.add(label[12]);
         label[12].addMouseListener(this);
         
@@ -162,7 +162,7 @@ public class historyadmin extends JFrame implements MouseListener{
         label[13].setOpaque(true);
         label[13].setForeground(Color.WHITE);
         label[13].setBounds(1080, 148, 200, 30);
-        label[13].setFont(f);
+        label[13].setFont(font1);
         panel.add(label[13]);
         label[13].addMouseListener(this);
         
@@ -176,7 +176,7 @@ public class historyadmin extends JFrame implements MouseListener{
             System.out.println("Connected");
             log = "SELECT * FROM customer WHERE Phone=?";
             ps = conn.prepareStatement(log);
-            ps.setString(1,t.getText());
+            ps.setString(1,textfield.getText());
             rs = ps.executeQuery();
             table.setModel(DbUtils.resultSetToTableModel(rs));
             conn.close();
@@ -222,7 +222,7 @@ public class historyadmin extends JFrame implements MouseListener{
             CustomerHistory();
         }
         if (me.getSource() == label[4]) {
-            t.setText("");
+            textfield.setText("");
             label[7].setText("");
             label[8].setText("");
             CustomerHistory();
@@ -233,7 +233,7 @@ public class historyadmin extends JFrame implements MouseListener{
                 conn=DriverManager.getConnection("JDBC:sqlite:tlecome.db");
                 log = "SELECT * FROM customer WHERE phone=?";
                 ps = conn.prepareStatement(log);
-                ps.setString(1,t.getText());
+                ps.setString(1,textfield.getText());
                 rs = ps.executeQuery();
                 if (rs.next()) {
                     label[7].setText(rs.getString("name"));
@@ -251,7 +251,7 @@ public class historyadmin extends JFrame implements MouseListener{
             try{
                 Class.forName("org.sqlite.JDBC");
                 conn=DriverManager.getConnection("JDBC:sqlite:tlecome.db");
-                log = "delete from customer where phone ="+t.getText();
+                log = "delete from customer where phone ="+textfield.getText();
                 ps = conn.prepareStatement(log);
                 ps.executeUpdate();
                 JOptionPane.showMessageDialog(null, "Record Deleted!");
@@ -259,7 +259,7 @@ public class historyadmin extends JFrame implements MouseListener{
             }catch(Exception e){
                 JOptionPane.showMessageDialog(null, e);
             }
-                t.setText("");
+                textfield.setText("");
                 label[7].setText("");
                 label[8].setText("");
                 CustomerHistory();
@@ -276,7 +276,7 @@ public class historyadmin extends JFrame implements MouseListener{
             }catch(Exception e){
                 JOptionPane.showMessageDialog(null, e);
             }
-                t.setText("");
+                textfield.setText("");
                 label[7].setText("");
                 label[8].setText("");
                 CustomerHistory();
@@ -316,10 +316,10 @@ public class historyadmin extends JFrame implements MouseListener{
     @Override
     public void mouseExited(MouseEvent me) {
         if (me.getSource() == label[1]) {
-            label[1].setBackground(c);
+            label[1].setBackground(color1);
         }
         if (me.getSource() == label[2]) {
-            label[2].setBackground(c);
+            label[2].setBackground(color1);
         }
     }
 }
